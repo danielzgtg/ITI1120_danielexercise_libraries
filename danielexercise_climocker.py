22c743186ba11891cf242da061f1bbb336f038a2
Daniel Tang <danielzgtg.opensource@gmail.com>
2018-11-18 06:30:49 -0500

    Fully implement print

--- danielexercise_climocker.py
+++ danielexercise_climocker.py
@@ -10,18 +10,30 @@ from danielexercise_abstractmocker import MockerBase
 class CLIMocker(MockerBase):
     _input_buf: deque
     _output_buf: deque
+    _output_incomplete: str
 
     def _init_buffers(self):
         self._input_buf = deque()
         self._output_buf = deque()
+        self._output_incomplete = ""
 
     def _install_all(self, tester):
         self._install_print(tester)
         self._install_input(tester)
 
     def _install_print(self, tester):
-        def fake_print(*args):
-            self._output_buf.append(" ".join(map(str, args)))
+        def fake_print(*args, sep=" ", end="\n", file=None, flush=False):
+            body = sep.join(map(str, args))
+            if end == "\n":
+                self._output_buf.append(self._output_incomplete + body)
+                self._output_incomplete = ""
+            else:
+                self._output_incomplete += body + end
+                if "\n" in self._output_incomplete:
+                    split = self._output_incomplete.split("\n")
+                    self._output_incomplete = split[-1]
+                    for complete in split[:-1]:
+                        self._output_buf.append(complete)
         self._backup_print = builtins.print
         builtins.print = fake_print
 
@@ -50,8 +62,8 @@ class CLIMocker(MockerBase):
             tester.assertTrue(self._output_buf)
             take = self._output_buf.popleft()
             if "\n" in take:
-                take2 = take.split("\n")
-                take = take2[0]
-                for extra in reversed(take2[1:]):
+                split = take.split("\n")
+                take = split[0]
+                for extra in reversed(split[1:]):
                     self._output_buf.appendleft(extra)
             tester.assertEqual(line, take)
